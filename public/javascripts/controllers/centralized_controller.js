4d59d81c0b8f9a0737c874ef929e1d0a7d693508
restcontmx <gunt.raro@gmail.com>
2017-12-10 22:37:53 -0600

    Centralized summary working

--- public/javascripts/controllers/centralized_controller.js
+++ public/javascripts/controllers/centralized_controller.js
@@ -1,32 +1,47 @@
 yukonApp
-    .controller('centralized-reports-controller', ['$scope',
-        '$rootScope',
-        'AuthRepository',
-        'LocationRepository',
-        'DashboardRepository',
-        'Excel',
-        '$timeout',
-        function ($scope,
-            $rootScope,
-            AuthRepository,
-            LocationRepository,
-            DashboardRepository,
-            Excel,
-            $timeout) {
+    .factory( 'CentralizedRepository', [ '$http', function( $http ) {
+        return ({
+            get_total_guests_per_locations : ( d1, d2 ) => $http.get( '/reports/guest/?d1=' + d1 + '&d2=' + d2 )
+        })
+    }])
+    .controller( 'centralized-reports-controller', [    '$scope',
+                                                        '$rootScope',
+                                                        'AuthRepository',
+                                                        'LocationRepository',
+                                                        'DashboardRepository',
+                                                        'CentralizedRepository',
+                                                        'Excel',
+                                                        '$timeout',
+                                                        'growl',
+                                                        function (  $scope,
+                                                                    $rootScope,
+                                                                    AuthRepository,
+                                                                    LocationRepository,
+                                                                    DashboardRepository,
+                                                                    CentralizedRepository,
+                                                                    Excel,
+                                                                    $timeout,
+                                                                    growl   ) {
 
-           
             $scope.full_table_display = true;
             $scope.progress_ban = false; // This is for the loanding simbols or whatever you want to activate
             $scope.users_info = $rootScope.user_info.first_name ? $rootScope.user_info.first_name + ' ' + $rootScope.user_info.last_name : $rootScope.user_info.email;
             $scope.current_year = new Date().getFullYear();
             $scope.last_year = new Date().getFullYear() - 1;
             $scope.target_percentage = 10/100;
+            
+            //
+            // date picker cofnig
+            //
             if ($("#dp_basic").length) {
                 $("#dp_basic").datepicker({
                     autoclose: true
                 });
             }
 
+            //
+            // get all the locations
+            //
             LocationRepository.getAll().success(function (response) {
                 if (!response.error) {
                     $scope.locations = response.data
@@ -36,9 +51,17 @@ yukonApp
             }).error(function (error) {
                 $scope.errors = error
             })
+
+            //
+            //  get_reports gets the reports
+            //  @params none
+            //  @returns none
+            //
             $scope.get_reports = function () {
-                $scope.progress_ban = true; // This is for the loanding simbols or whatever you want to activate
-                let date_f = document.getElementById('dp_basic').value
+
+                let date_f = document.getElementById('dp_basic').value             
+                $scope.progress_ban = true;
+
                 if (date_f) {
 
                     dailyReports(date_f)
@@ -47,26 +70,32 @@ yukonApp
                         monthlyReports(date_f)
                     }, 1000)
 
-                    $timeout(function () {
-                        yearlyReports(date_f)
-                    }, 2000)
+                    yearlyReports(date_f)
                     
                 }
             }
+
+            //
+            //  dailyReports gets the daly reports by end date
+            //  @param date_f end date
+            //  @returns none
+            //
             function dailyReports(date_f) {
+
+                let selected_date = new Date(date_f),
+                    daily_date_s = date_f + ' 00:00:00',
+                    daily_date_e = date_f + ' 23:59:59',
+                    last_year_daily_date_e = (selected_date.getMonth() + 1) + '/' + selected_date.getDate() + '/' + (selected_date.getFullYear() - 1) + ' 23:59:59',
+                    last_year_daily_date_s = (selected_date.getMonth() + 1) + '/' + selected_date.getDate() + '/' + (selected_date.getFullYear() - 1) + ' 00:00:00'
+
                 $scope.the_date = new Date(date_f)
-                let selected_date = new Date(date_f)
                 $scope.current_year = selected_date.getFullYear();
                 $scope.last_year = selected_date.getFullYear() - 1;
-                let daily_date_s = date_f + ' 00:00:00',
-                    daily_date_e = date_f + ' 23:59:59'
-                let last_year_daily_date_e = (selected_date.getMonth() + 1) + '/' + selected_date.getDate() + '/' + (selected_date.getFullYear() - 1) + ' 23:59:59'
-                let last_year_daily_date_s = (selected_date.getMonth() + 1) + '/' + selected_date.getDate() + '/' + (selected_date.getFullYear() - 1) + ' 00:00:00'
 
                 DashboardRepository.get_sales_by_dates_locations(daily_date_s, daily_date_e).success(function (response) {
                     if (!response.error) {
                         $scope.locations_sales = response.data
-                        DashboardRepository.get_totals_by_dates_locations(daily_date_s, daily_date_e).success(function (r_t) {
+                        CentralizedRepository.get_total_guests_per_locations(daily_date_s, daily_date_e).success(function (r_t) {
                             if (!r_t.error) {
                                 $scope.locations.forEach(l => {
                                     l.guests = r_t.data.total_guests.filter(loc => loc.location == l.id).reduce((a, b) => (a + b.total_guests), 0)
@@ -93,7 +122,7 @@ yukonApp
                 DashboardRepository.get_sales_by_dates_locations(last_year_daily_date_s, last_year_daily_date_e).success(function (response2) {
                     if (!response2.error) {
                         $scope.l_y_locations_sales = response2.data
-                        DashboardRepository.get_totals_by_dates_locations(last_year_daily_date_s, last_year_daily_date_e).success(function (r_t) {
+                        CentralizedRepository.get_total_guests_per_locations(last_year_daily_date_s, last_year_daily_date_e).success(function (r_t) {
                             if (!r_t.error) {
                                 $scope.locations.forEach(l => {
                                     l.l_y_guests = r_t.data.total_guests.filter(loc => loc.location == l.id).reduce((a, b) => (a + b.total_guests), 0)
@@ -114,19 +143,25 @@ yukonApp
                     }
                 }).error(function (error) {
                 });
-
             }
+
+            //
+            //  monthlyReports gets the monthly reports by end date
+            //  @params date_f the end date
+            //  @return none
+            //
             function monthlyReports(date_f) {
+
                 let selected_date = new Date(date_f)
-                let daily_date_e = date_f + ' 23:59:59'
-                let monthly_date_s = (selected_date.getMonth() + 1) + '/01/' + selected_date.getFullYear() + ' 00:00:00'
-                let last_year_daily_date_e = (selected_date.getMonth() + 1) + '/' + selected_date.getDate() + '/' + (selected_date.getFullYear() - 1) + ' 23:59:59'
-                let last_year_monthly_date_s = (selected_date.getMonth() + 1) + '/01/' + (selected_date.getFullYear() - 1) + ' 00:00:00'
+                    daily_date_e = date_f + ' 23:59:59',
+                    monthly_date_s = (selected_date.getMonth() + 1) + '/01/' + selected_date.getFullYear() + ' 00:00:00',
+                    last_year_daily_date_e = (selected_date.getMonth() + 1) + '/' + selected_date.getDate() + '/' + (selected_date.getFullYear() - 1) + ' 23:59:59',
+                    last_year_monthly_date_s = (selected_date.getMonth() + 1) + '/01/' + (selected_date.getFullYear() - 1) + ' 00:00:00'
 
                 DashboardRepository.get_sales_by_dates_locations(monthly_date_s, daily_date_e).success(function (response) {
                     if (!response.error) {
                         $scope.m_locations_sales = response.data
-                        DashboardRepository.get_totals_by_dates_locations(monthly_date_s, daily_date_e).success(function (r_t) {
+                        CentralizedRepository.get_total_guests_per_locations(monthly_date_s, daily_date_e).success(function (r_t) {
                             if (!r_t.error) {
                                 $scope.locations.forEach(l => {
                                     l.m_total_sales = $scope.m_locations_sales.sale_reports.filter(loc => loc.location == l.id).reduce((a, b) => (a + b.total), 0)
@@ -151,7 +186,7 @@ yukonApp
                 DashboardRepository.get_sales_by_dates_locations(last_year_monthly_date_s, last_year_daily_date_e).success(function (response2) {
                     if (!response2.error) {
                         $scope.l_y_m_locations_sales = response2.data
-                        DashboardRepository.get_totals_by_dates_locations(last_year_monthly_date_s, last_year_daily_date_e).success(function (r_t) {
+                        CentralizedRepository.get_total_guests_per_locations(last_year_monthly_date_s, last_year_daily_date_e).success(function (r_t) {
                             if (!r_t.error) {
                                 $scope.locations.forEach(l => {
                                     l.l_y_m_total_sales = $scope.l_y_m_locations_sales.sale_reports.filter(loc => loc.location == l.id).reduce((a, b) => (a + b.total), 0)
@@ -173,73 +208,85 @@ yukonApp
                 }).error(function (error) {
                 });
             }
-            // Get the yearly reports by date
+
+            // 
+            //  yearlyReports gets the yearly reports by date
+            //  @param date_f the end date
+            //  @returns none
+            //  
             function yearlyReports(date_f) {
+
                 let selected_date = new Date(date_f)
-                let daily_date_e = date_f + ' 23:59:59'
-                let yearly_date_s = '01/01/' + selected_date.getFullYear() + ' 00:00:00'
-                let last_year_daily_date_e = (selected_date.getMonth() + 1) + '/' + selected_date.getDate() + '/' + (selected_date.getFullYear() - 1) + ' 23:59:59'
-                let last_year_yearly_date_s = '01/01/' + (selected_date.getFullYear() - 1) + ' 00:00:00'
+                    daily_date_e = date_f + ' 23:59:59',
+                    yearly_date_s = '01/01/' + selected_date.getFullYear() + ' 00:00:00',
+                    last_year_daily_date_e = (selected_date.getMonth() + 1) + '/' + selected_date.getDate() + '/' + (selected_date.getFullYear() - 1) + ' 23:59:59',
+                    last_year_yearly_date_s = '01/01/' + (selected_date.getFullYear() - 1) + ' 00:00:00'
 
-                DashboardRepository.get_sales_by_dates_locations(yearly_date_s, daily_date_e).success(function (response) {
-                    if (!response.error) {
-                        $scope.y_locations_sales = response.data
-                        DashboardRepository.get_totals_by_dates_locations(yearly_date_s, daily_date_e).success(function (r_t) {
-                            if (!r_t.error) {
-                                $scope.locations.forEach(l => {
-                                    l.y_total_sales = $scope.y_locations_sales.sale_reports.filter(loc => loc.location == l.id).reduce((a, b) => (a + b.total), 0)
-                                    l.y_guests = r_t.data.total_guests.filter(loc => loc.location == l.id).reduce((a, b) => (a + b.total_guests), 0)
-                                    l.y_ticket_avg = l.y_guests > 0 ? l.y_total_sales / l.y_guests : 0
-                                });
-                                $scope.yearly_ticket_avg_total = $scope.locations.reduce((a, b) => (a + b.y_ticket_avg), 0)
-                                $scope.yearly_sales_total = $scope.locations.reduce((a, b) => (a + b.y_total_sales), 0)
-                                $scope.yearly_guests_total = $scope.locations.reduce((a, b) => (a + b.y_guests), 0)
-                            } else {
-                                console.log(r_t.message)
-                            }
-                        }).error(function (error) {
-                            console.log(error)
-                        })
-                    } else {
-                        console.log(response.message)
-                    }
-                }).error(function (error) {
-                });
+                $timeout(function () {
+                    DashboardRepository.get_sales_by_dates_locations(yearly_date_s, daily_date_e).success(function (response) {
+                        if (!response.error) {
+                            $scope.y_locations_sales = response.data
+                            CentralizedRepository.get_total_guests_per_locations(yearly_date_s, daily_date_e).success(function (r_t) {
+                                if (!r_t.error) {
+                                    $scope.locations.forEach(l => {
+                                        l.y_total_sales = $scope.y_locations_sales.sale_reports.filter(loc => loc.location == l.id).reduce((a, b) => (a + b.total), 0)
+                                        l.y_guests = r_t.data.total_guests.filter(loc => loc.location == l.id).reduce((a, b) => (a + b.total_guests), 0)
+                                        l.y_ticket_avg = l.y_guests > 0 ? l.y_total_sales / l.y_guests : 0
+                                    });
+                                    $scope.yearly_ticket_avg_total = $scope.locations.reduce((a, b) => (a + b.y_ticket_avg), 0)
+                                    $scope.yearly_sales_total = $scope.locations.reduce((a, b) => (a + b.y_total_sales), 0)
+                                    $scope.yearly_guests_total = $scope.locations.reduce((a, b) => (a + b.y_guests), 0)
+                                } else {
+                                    console.log(r_t.message)
+                                }
+                            }).error(function (error) {
+                                console.log(error)
+                            })
+                        } else {
+                            console.log(response.message)
+                        }
+                    }).error(function (error) {
+                    });
+                }, 2000 );
 
-                DashboardRepository.get_sales_by_dates_locations(last_year_yearly_date_s, last_year_daily_date_e).success(function (response2) {
-                    if (!response2.error) {
-                        $scope.l_y_y_locations_sales = response2.data
-                        DashboardRepository.get_totals_by_dates_locations(last_year_yearly_date_s, last_year_daily_date_e).success(function (r_t) {
-                            if (!r_t.error) {
-                                $scope.locations.forEach(l => {
-                                    l.l_y_y_total_sales = $scope.l_y_y_locations_sales.sale_reports.filter(loc => loc.location == l.id).reduce((a, b) => (a + b.total), 0)
-                                    l.l_y_y_guests = r_t.data.total_guests.filter(loc => loc.location == l.id).reduce((a, b) => (a + b.total_guests), 0)
-                                    l.l_y_y_ticket_avg = l.l_y_y_guests > 0 ? l.l_y_y_total_sales / l.l_y_y_guests : 0
-                                });
-                                $scope.l_y_yearly_guests_total = $scope.locations.reduce((a, b) => (a + b.l_y_y_guests), 0)
-                                $scope.l_y_yearly_sales_total = $scope.locations.reduce((a, b) => (a + b.l_y_y_total_sales), 0)
-                                $scope.l_y_yearly_ticket_avg_total = $scope.locations.reduce((a, b) => (a + b.l_y_y_ticket_avg), 0)
-                            } else {
-                                console.log(r_t.message)
-                            }
-                            $scope.progress_ban = false
-                        }).error(function (error) {
-                            $scope.progress_ban = false
-                        })
-                    } else {
-                        console.log(response2.message)
-                    }
-                }).error(function (error) {
-                });
+                $timeout(function() {
+                    DashboardRepository.get_sales_by_dates_locations(last_year_yearly_date_s, last_year_daily_date_e).success(function (response2) {
+                        if (!response2.error) {
+                            $scope.l_y_y_locations_sales = response2.data
+                            CentralizedRepository.get_total_guests_per_locations(last_year_yearly_date_s, last_year_daily_date_e).success(function (r_t) {
+                                if (!r_t.error) {
+                                    $scope.locations.forEach(l => {
+                                        l.l_y_y_total_sales = $scope.l_y_y_locations_sales.sale_reports.filter(loc => loc.location == l.id).reduce((a, b) => (a + b.total), 0)
+                                        l.l_y_y_guests = r_t.data.total_guests.filter(loc => loc.location == l.id).reduce((a, b) => (a + b.total_guests), 0)
+                                        l.l_y_y_ticket_avg = l.l_y_y_guests > 0 ? l.l_y_y_total_sales / l.l_y_y_guests : 0
+                                    });
+                                    $scope.l_y_yearly_guests_total = $scope.locations.reduce((a, b) => (a + b.l_y_y_guests), 0)
+                                    $scope.l_y_yearly_sales_total = $scope.locations.reduce((a, b) => (a + b.l_y_y_total_sales), 0)
+                                    $scope.l_y_yearly_ticket_avg_total = $scope.locations.reduce((a, b) => (a + b.l_y_y_ticket_avg), 0)
+                                } else {
+                                    console.log(r_t.message)
+                                }
+                                $scope.progress_ban = false
+                            }).error(function (error) {
+                                $scope.progress_ban = false
+                            })
+                        } else {
+                            console.log(response2.message)
+                        }
+                    }).error(function (error) {
+                    });
+                }, 4000 );
             }
 
-
-
+            //
+            // print_reports prints the reports on an excel sheet
+            // @params none
+            // @returns none
+            //
             $scope.print_reports = function () {
                 var exportHref = Excel.tableToExcel('#export_table', 'WireWorkbenchDataExport');
                 $timeout(function () {
                     location.href = exportHref;
                 }, 100);
             }
-
         }])
--- routes/reports.js
+++ routes/reports.js
@@ -180,6 +180,25 @@ router.get( '/void/', jsonParser, function( req, res ) {
 });
 
 /**
+* guest reports pettition
+**/
+router.get( '/guest/', jsonParser, function( req, res ) {
+    var userdata = JSON.parse( req.cookies['userdata'] ),
+        url_parts = urlLib.parse( req.url, true );
+    request(
+        {
+            url : http_helper.get_business_api_uri( userdata.user_data.e_p, 'guest/reports/', '?d1=' + url_parts.query.d1 + '&d2=' + url_parts.query.d2 ),
+            method : 'GET',
+            json : true,
+            headers : {
+                'Authorization' : http_helper.get_basic_auth_w_token( encryption_system.decryptCookie( userdata.auth_data ) )
+            }
+        },
+        ( error, response, body ) => { res.send( http_helper.data_format_ok( error, response, body ) ) }
+    );
+});
+
+/**
 * petty cash reports pettition
 **/
 router.get( '/pettycash/', jsonParser, function( req, res ) {
