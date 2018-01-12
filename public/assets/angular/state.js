d75b9eb7e74e6e298e8cc326e5db2e9fe96e7dea
Ramon Octavio Badillo Rivas <robr8@hotmail.com>
2018-01-12 03:43:38 -0600

    ui change

--- public/assets/angular/state.js
+++ public/assets/angular/state.js
@@ -67,7 +67,10 @@ yukonApp
                                     'assets/lib/selectize-js/js/standalone/selectize.min.js',
                                     'https://cdn.jsdelivr.net/alasql/0.3/alasql.min.js',
                                     'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.7.12/xlsx.core.min.js',
-                                    'assets/lib/jquery-qrcode-0.10.1/jquery.qrcode-0.10.1.min.js'
+                                    'assets/lib/jquery-qrcode-0.10.1/jquery.qrcode-0.10.1.min.js',
+                                    'assets/lib/bootstrap-datepicker/css/datepicker3.css',
+                                    'assets/lib/bootstrap-datepicker/js/bootstrap-datepicker.js'
+
                                 ]);
                             }
                         ]
@@ -2314,7 +2317,8 @@ yukonApp
                                     'assets/lib/selectize-js/js/standalone/selectize.min.js',
                                     'https://cdn.jsdelivr.net/alasql/0.3/alasql.min.js',
                                     'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.7.12/xlsx.core.min.js',
-                                    'assets/lib/jquery-qrcode-0.10.1/jquery.qrcode-0.10.1.min.js'
+                                    'assets/lib/jquery-qrcode-0.10.1/jquery.qrcode-0.10.1.min.js',
+                                    'assets/lib/bootstrap-datepicker/js/bootstrap-datepicker.js'
                                     
                                 ])
                             }
